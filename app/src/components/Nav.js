ebe139fcb8fba1ee1c90cf3d4edea5d608b72b3c
Raymond Robinson <rayissach@gmail.com>
2024-06-05 20:01:04 -0400

    Initializing Tailwind css

--- app/src/components/Nav.js
+++ app/src/components/Nav.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import logo from '../images/ray_gif_logo02.gif'
 
 export const Nav = () => {
     return (
-        <h1>
-            Hello World
-        </h1>
+        <nav className='bg-gray-800'>
+            <div>
+                <div className='items-stretch'>
+                    <div className='size-20'>
+                        <img alt='logo' src={logo} />
+                    </div>
+                    <ul className='list-none'>
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Portfolio</li>
+                        <li>Contact</li>
+                    </ul>
+                </div>
+            </div> 
+        </nav>
     )
 }
