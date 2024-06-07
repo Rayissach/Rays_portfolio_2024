a433337fa0c3303ba2b48ce62f43f62643aad09c
Raymond Robinson <rayissach@gmail.com>
2024-06-07 12:54:24 -0400

    working on navbar with brand logo

--- app/src/components/Nav.js
+++ app/src/components/Nav.js
@@ -1,22 +1,20 @@
 import React from 'react'
-import logo from '../images/ray_gif_logo02.gif'
+import logo from '../images/gif_logo_4.gif'
 
 export const Nav = () => {
     return (
-        <nav className='bg-gray-800'>
-            <div>
-                <div className='items-stretch'>
-                    <div className='size-20 bg-gray-800 rflex flex-1'>
-                        <img alt='logo' className='bg-gray-800' src={logo} />
-                    </div>
-                    <ul className='list-none flex space-x-4'>
-                        <li>Home</li>
-                        <li>About</li>
-                        <li>Portfolio</li>
-                        <li>Contact</li>
-                    </ul>
-                </div>
-            </div> 
-        </nav>
+        <header>
+      <nav>
+        <div className='logo'>
+            <img src={logo} alt='Rays Logo'/>
+        </div>
+        <ul>
+          <li><a href="#hero">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#projects">Portfolio</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </nav>
+        </header>
     )
 }
