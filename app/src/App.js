a2b2a771b978d64defa26fe6504929b13d178199
Raymond Robinson <rayissach@gmail.com>
2024-06-08 17:24:28 -0400

    creating and modifying home.js component

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import { Nav } from './components/Nav' 
+import { Home } from './components/Home'
 
 function App() {
   return (
-    <div className="App">
+    <div className="App max-h-screen">
       <Nav />
+      <Home />
     </div>
   );
 }
---
+++ app/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Home = () => {
+    return (
+        <div className='bg-slate-900 flex items-center justify-center max-h-screen h-screen'>
+            <div className='text-slate-300 place-content-center'>
+                <h1>Hi, my name is Ray</h1>
+                <p>and welcome to my portfolio</p>
+                <p>enter my world of creativity and thoughts..</p>
+            </div>
+        </div>
+    )
+}
