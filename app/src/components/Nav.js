b72564fe72ecfcf9e3bdf9233a2a0a550d7edd19
Raymond Robinson <rayissach@gmail.com>
2024-06-11 09:22:34 -0400

    improving navbar edits

--- app/src/components/Nav.js
+++ app/src/components/Nav.js
@@ -4,12 +4,12 @@ import logo from '../images/gif_logo_4.gif'
 export const Nav = () => {
     return (
         <header>
-            <nav className='w-screen'>
-                <div className='logo float-left'>
-                    <img src={logo} alt='Rays Logo'/>
+            <nav className='w-screen flex items-center justify-center px-4'>
+                <div className='logo'>
+                    <img src={logo} alt='Rays Logo' className='h-12'/>
                 </div>
-                <div >
-                    <ul className='flex items-center justify-center py-3'>
+                <div className='border-2 flex-grow flex justify-center'>
+                    <ul className='flex items-center space-x-8 py-3'>
                     <li><a href="#hero">Home</a></li>
                     <li><a href="#about">About</a></li>
                     <li><a href="#projects">Portfolio</a></li>
