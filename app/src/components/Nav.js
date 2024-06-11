d9d0cfa643d1bcf56c01fb8bf1e4cc50a1849452
Raymond Robinson <rayissach@gmail.com>
2024-06-11 09:08:12 -0400

    aligning navbar items

--- app/src/components/Nav.js
+++ app/src/components/Nav.js
@@ -4,17 +4,19 @@ import logo from '../images/gif_logo_4.gif'
 export const Nav = () => {
     return (
         <header>
-      <nav>
-        <div className='logo'>
-            <img src={logo} alt='Rays Logo'/>
-        </div>
-        <ul>
-          <li><a href="#hero">Home</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#projects">Portfolio</a></li>
-          <li><a href="#contact">Contact</a></li>
-        </ul>
-      </nav>
+            <nav className='w-screen'>
+                <div className='logo float-left'>
+                    <img src={logo} alt='Rays Logo'/>
+                </div>
+                <div >
+                    <ul className='flex items-center justify-center py-3'>
+                    <li><a href="#hero">Home</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#projects">Portfolio</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                    </ul>
+                </div>
+            </nav>
         </header>
     )
 }
