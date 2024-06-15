0333041069ebde6e9344483d1985aa99b0d97833
Raymond Robinson <rayissach@gmail.com>
2024-06-15 09:15:37 -0400

    adding gif to Home background

--- app/src/components/Home.js
+++ app/src/components/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Home = () => {
     return (
-        <div className='bg-slate-900 flex items-center justify-center max-h-screen h-screen'>
+        <div className='background-gif flex items-center justify-center max-h-screen h-screen'>
             <div className='text-slate-300 place-content-center'>
                 <h1>Hi, my name is Ray</h1>
                 <p>and welcome to my portfolio</p>
