a972c38669bb0acfc06633930396144b708d93db
Raymond Robinson <rayissach@gmail.com>
2024-06-17 12:39:57 -0400

    Editing nav bar items positions

--- app/src/components/Nav.js
+++ app/src/components/Nav.js
@@ -8,12 +8,12 @@ export const Nav = () => {
                 <div className='logo'>
                     <img src={logo} alt='Rays Logo' className='h-12'/>
                 </div>
-                <div className='border-2 border-slate-500 flex-grow flex justify-center rounded-full bg-transparent'>
-                    <ul className='flex items-center space-x-8 py-3'>
-                    <li><a href="#hero">Home</a></li>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#projects">Portfolio</a></li>
-                    <li><a href="#contact">Contact</a></li>
+                <div className='border-2 border-slate-500 flex-grow flex justify-center rounded-full bg-transparent text-xs '>
+                    <ul className='flex items-center space-x-8 py-3 px-5'>
+                        <li className=' hover:text-base hover:px-0 hover:py-0 '><a className='hover:no-underline' href="#hero">Home</a></li>
+                        <li className='hover:text-base hover:px-0 hover:py-0 '><a className='hover:no-underline' href="#about">About</a></li>
+                        <li className='hover:text-base hover:px-0 hover:py-0 '><a className='hover:no-underline' href="#projects">Portfolio</a></li>
+                        <li className='hover:text-base hover:px-0 hover:py-0 '><a className='hover:no-underline' href="#contact">Contact</a></li>
                     </ul>
                 </div>
             </nav>
